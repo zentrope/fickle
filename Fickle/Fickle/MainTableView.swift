c25152f51531e803575763adbdb090b9bfcc1a98
Keith Irwin <keith@zentrope.com>
2019-04-08 00:51:26 -0700

    Experiment with image formats for table rows

--- Fickle/Fickle/MainTableView.swift
+++ Fickle/Fickle/MainTableView.swift
@@ -37,7 +37,9 @@ class MainTableView: NSView {
         tableView.gridStyleMask = .solidHorizontalGridLineMask
         tableView.selectionHighlightStyle = .regular
         tableView.headerView = nil
-        
+        //tableView.usesAutomaticRowHeights = false
+        tableView.rowSizeStyle = NSTableView.RowSizeStyle.custom
+
         let column = NSTableColumn(identifier: NSUserInterfaceItemIdentifier("column"))
         column.isEditable = false
 
@@ -69,6 +71,7 @@ class MainTableView: NSView {
 }
 
 extension MainTableView: NSTableViewDelegate {
+
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
 
         // Just return the row to see if all this works.
@@ -81,21 +84,39 @@ extension MainTableView: NSTableViewDelegate {
         field.drawsBackground = false
 
         field.stringValue = "\(row)"
+        field.sizeToFit()
 
+        let image = NSImageView()
+        // Can't explicitly name files when in AppSandbox mode.
+        //image.image = NSImage(byReferencing: URL(fileURLWithPath: "/Users/foo/bar.png"))
+        image.sizeThatFits(NSMakeSize(200, 100))
+        image.imageScaling = NSImageScaling.scaleAxesIndependently
+        image.wantsLayer = true
+        image.layer?.backgroundColor = NSColor.systemGreen.cgColor
+
+        image.sizeToFit()
         let view = NSView(frame: NSZeroRect)
+        view.addSubview(image)
         view.addSubview(field)
+
+        constrain(image, [
+            image.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            image.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 7),
+            image.widthAnchor.constraint(equalToConstant: 200),
+            image.heightAnchor.constraint(equalToConstant: 100)
+            ])
 
         constrain(field, [
             field.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            field.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 7),
-            field.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -7),
+            field.leadingAnchor.constraint(equalTo: image.trailingAnchor, constant: 7),
+            field.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -7)
         ])
 
         return view
     }
 
     func tableView(_ tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
-        return 32
+        return 110
     }
 }
 
