ab2f198697b0e6ec5b814d3358e90772821061ec
Keith Irwin <keith@zentrope.com>
2019-05-07 16:00:15 -0700

    Shrink theme-setter

--- Fickle/Fickle/Views/ThemeImageView.swift
+++ Fickle/Fickle/Views/ThemeImageView.swift
@@ -14,7 +14,7 @@ class ThemeImageView: NSImageView {
     private let white = NSColor.white.cgColor
 
     var theme: Theme?
-    var badgeRect: CGRect { return CGRect(x: bounds.maxX - 30, y: bounds.maxY - 30, width: 20, height: 20) }
+    var badgeRect: CGRect { return CGRect(x: bounds.maxX - 18, y: bounds.maxY - 18, width: 12, height: 12) }
 
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
@@ -31,7 +31,7 @@ class ThemeImageView: NSImageView {
 
         ctx.setStrokeColor(theme.appearance == .light ? black : white)
         ctx.setFillColor(theme.appearance == .light ? white : black)
-        ctx.setAlpha(0.8)
+        //ctx.setAlpha(0.8)
         ctx.setLineWidth(1)
         ctx.addEllipse(in: badgeRect)
         ctx.fillEllipse(in: badgeRect)
